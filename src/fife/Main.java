27b638cd2819d8ef08201929fd5c66823189c8c9
Tori Fife <torifife@gmail.com>
2019-05-14 13:37:10 -0600

    add second print message to see if I can figure this out at all

--- src/fife/Main.java
+++ src/fife/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hello my name is Tori.");
+	System.out.println("This is a change to see if it changes.");
     }
 }
