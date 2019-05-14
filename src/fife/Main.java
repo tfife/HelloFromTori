ce9288499fe277d9cb141d8ab3a514e3c4563b7b
jordanburdett <jordan@burdett.us>
2019-05-14 13:50:08 -0600

    Said hi...:)

--- src/fife/Main.java
+++ src/fife/Main.java
@@ -3,6 +3,8 @@ package fife;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello my name is Tori.");
+
+        System.out.println("Hello my name is Tori.");
+        System.out.println("Hello tori, My name is Jordan");
     }
 }
